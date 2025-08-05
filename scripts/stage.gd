3534782c9dc55492e672d45a1c6074702de8fee6
DazyLude <36658009+DazyLude@users.noreply.github.com>
2025-08-05 09:50:34 +0400

    added fonts added crowki sprites added checkpoint sprite and replace the old bg

--- scripts/stage.gd
+++ scripts/stage.gd
@@ -39,7 +39,7 @@ static func from_dict(params: Dictionary) -> StageData:
 	
 	# other parameters
 	data.stage_length = params.get("stage length", 3e4);
-	data.intermission = load(params.get("intermission", "uid://ct1qkka6lngc5"));
+	data.intermission = load(params.get("intermission", "res://assets/stages/farm/checkpoint_juicestand.png"));
 	
 	return data;
 
@@ -49,9 +49,9 @@ static var stage_variants: Dictionary[String, Dictionary] = {
 	"tomato fields": {
 		"terrain fill": "uid://dftdfph41cnpv", # can be a relative path to the texture, but a string nevertheless
 		"terrain edge": "uid://bad5ey7wkh63x", # same as "terrain fill"
-		"checkpoint": "uid://d28lqabm420p3", # same as "terrain fill"
+		"checkpoint": "res://assets/stages/farm/checkpoint_juicestand.png", # same as "terrain fill"
 		"terrain scale": Vector2(1e-3, 1e2), # should be a Vector2
 		"stage length": 3e4, # should be a float
-		"intermission": "uid://ct1qkka6lngc5", # can be a relative path to the texture, but a string nevertheless
+		"intermission": "uid://ct1qkka6lngc5", # can be a relative path to the scene, but a string nevertheless
 	}
 }
