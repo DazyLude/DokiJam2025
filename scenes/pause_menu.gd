35eb294f47ca9038610d4378dee372145fd0839f
DazyLude <36658009+DazyLude@users.noreply.github.com>
2025-08-09 19:24:12 +0400

    added "no coffee" sign at the left end of the map pause menu launches in the game

--- scenes/pause_menu.gd
+++ scenes/pause_menu.gd
@@ -3,6 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# pause here
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	get_node("CoinContainer/CoinCount").text = "x%d" % GameState.dokicoins
 
@@ -10,3 +11,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed(&"pause"):
+		# unpause here or in another place the menu is exited
+		get_viewport().set_input_as_handled();
+		queue_free();
