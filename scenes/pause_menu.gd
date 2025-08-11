5b88c6edbeb637cb1586dbaa59b08690b4a649f9
LMG3864 <cristian3864@gmail.com>
2025-08-11 14:50:53 -0500

    Pause menu functionality
    -Added functionality to the pause menu.
    -Added save and load functionality to volume settings.

--- scenes/pause_menu.gd
+++ scenes/pause_menu.gd
@@ -1,11 +1,40 @@
 extends Control
 
+enum {
+	#MASTER,
+	MUSIC,
+	SFX
+}
+
+var slider_data: Dictionary[int, Dictionary] = {
+	MUSIC: {
+		"node": -1,
+		"bus": -1
+	},
+	SFX: {
+		"node": -1,
+		"bus": -1
+	},
+}
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# pause here
 	process_mode = Node.PROCESS_MODE_ALWAYS
+	get_tree().paused = true
+	# Initialize slider data dictionary
+	slider_data[MUSIC]["node"] = get_node("MusicContainer/VolumeSlider")
+	slider_data[MUSIC]["bus"] = AudioServer.get_bus_index("music")
+	slider_data[SFX]["node"] = get_node("SFXContainer/VolumeSlider")
+	slider_data[SFX]["bus"] = AudioServer.get_bus_index("sfx")
+	# Update Coin Display
 	get_node("CoinContainer/CoinCount").text = "x%d" % GameState.dokicoins
+	# Update slider data
+	update_slider_value(MUSIC)
+	update_slider_value(SFX)
+	# Update Ketchup Meter
+	get_node("KetchupMeter").value = GameState.juice/GameState.juice_cap * 100
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,5 +45,65 @@ func _process(delta: float) -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed(&"pause"):
 		# unpause here or in another place the menu is exited
-		get_viewport().set_input_as_handled();
-		queue_free();
+		close_pause_menu()
+
+
+func close_pause_menu() -> void:
+	get_tree().paused = false
+	get_viewport().set_input_as_handled();
+	queue_free();
+
+## update the value of the slider directly
+func update_slider_value(container_id: int) -> void:
+	var slider = slider_data[container_id]
+	slider["node"].value = AudioServer.get_bus_volume_linear(slider["bus"]) * 100
+
+## update the volume of a bus with a new value
+func update_volume_value(container_id: int, new_value: float) -> void:
+	var slider = slider_data[container_id]
+	new_value = snapped(new_value/100, 0.01)
+	AudioServer.set_bus_volume_linear(slider["bus"], new_value)
+
+## saves the value of the audio bus
+func save_volume_value(container_id: int) -> void:
+	var save_file: Object
+	var volume_data: Dictionary
+	var json_string: String
+	if OS.is_userfs_persistent():
+		save_file = FileAccess.open(GameState.options_file, FileAccess.WRITE)
+		volume_data = {
+			"music": slider_data[MUSIC]["node"].value,
+			"sfx": slider_data[SFX]["node"].value
+		}
+		json_string = JSON.stringify(volume_data)
+		save_file.store_string(json_string)
+		
+		print("Saved volume value")
+
+## change the music volume slider
+func _on_music_volume_changed(value: float) -> void:
+	update_volume_value(MUSIC, value)
+
+## change the sfx volume slider
+func _on_sfx_volume_changed(value: float) -> void:
+	update_volume_value(SFX, value)
+
+## stop moving the music volume slider
+func _on_music_volume_drag_ended(value_changed: bool) -> void:
+	if value_changed:
+		save_volume_value(MUSIC)
+
+## stop moving the sfx volume slider
+func _on_sfx_volume_drag_ended(value_changed: bool) -> void:
+	if value_changed:
+		save_volume_value(SFX)
+
+## closes the pause menu and resumes the game
+func _on_resume_pressed() -> void:
+	close_pause_menu()
+
+## exit the game back to the main menu
+func _on_menu_pressed() -> void:
+	get_tree().paused = false
+	#get_viewport().set_input_as_handled();
+	get_tree().change_scene_to_file("res://scenes/menu.tscn");
--- scripts/gamestate.gd
+++ scripts/gamestate.gd
@@ -5,6 +5,10 @@ extends Node
 # it holds data related to the current game session
 
 
+# Options file
+var options_file: String = "user://options.ini"
+
+
 # RNG
 var rng := RandomNumberGenerator.new();
 
@@ -32,6 +36,10 @@ var player : Player;
 var current_stage := StageData.get_stage_data_by_name("tomato fields");
 
 
+func _init() -> void:
+	load_options_file()
+
+
 func restart() -> void:
 	juice = juice_cap + 10.0 * upgrades.get_upgrade_level(Upgrade.KETCHUP_TANK);
 	current_stage.rng.state = current_stage.fresh_state;
@@ -42,3 +50,36 @@ func load_stage(stage: String) -> void:
 	upgrades.check_for_unlocks(stage);
 	current_stage = StageData.get_stage_data_by_name(stage);
 	get_tree().change_scene_to_file("res://scenes/game.tscn");
+
+
+func load_options_file() -> void:
+	var save_file: Object
+	var json: Object
+	var save_data: Dictionary
+	var json_string: String
+	if FileAccess.file_exists(options_file):
+		save_file = FileAccess.open(options_file, FileAccess.READ)
+		json = JSON.new()
+		json_string = save_file.get_line()
+		var error = json.parse(json_string)
+		if error == OK:
+			save_data = json.data
+			load_bus_volume("music", save_data["music"])
+			load_bus_volume("sfx", save_data["sfx"])
+		else:
+			print("ERROR - Could not parse options file")
+	else:
+		print("No options file detected")
+		# LMG Note: idea to create an options file on boot up
+		#save_file = FileAccess.open(options_file, FileAccess.WRITE)
+		#save_data = {
+		#	"music": 30,
+		#	"sfx": 100
+		#}
+		#var json_string = JSON.stringify(save_data)
+		#save_file.store_string(json_string)
+
+func load_bus_volume(bus_name: String, volume_value) -> void:
+	var volume = snapped(volume_value/100, 0.01)
+	var bus = AudioServer.get_bus_index(bus_name)
+	AudioServer.set_bus_volume_linear(bus, volume)
