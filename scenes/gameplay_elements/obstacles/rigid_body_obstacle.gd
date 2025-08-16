7b52252d5e99123ef33a8ec41aab1282f6fb79c4
DazyLude <36658009+DazyLude@users.noreply.github.com>
2025-08-16 19:57:22 +0400

    garbage spewer fixes

--- scenes/gameplay_elements/obstacles/rigid_body_obstacle.gd
+++ scenes/gameplay_elements/obstacles/rigid_body_obstacle.gd
@@ -12,7 +12,13 @@ func set_custom_scale(new_scale: Vector2) -> void:
 	$CollisionPolygon2D.scale = new_scale
 
 
-
 func flip() -> void:
 	$Sprite2D.scale *= Vector2(-1.0, 1.0);
 	$CollisionPolygon2D.scale *= Vector2(-1.0, 1.0);
+
+
+func _physics_process(delta: float) -> void:
+	if not get_collision_layer_value(1):
+		# check if can manifest
+		if get_colliding_bodies().is_empty():
+			set_collision_layer_value(1, true);
--- scenes/gameplay_elements/obstacles/spawning_obstacle.gd
+++ scenes/gameplay_elements/obstacles/spawning_obstacle.gd
@@ -1,25 +1,36 @@
 extends GenericObstacle
 class_name SpawningObstacle
 
-@onready var area : Area2D = $Area2D;
+@onready var area : Area2D = $Sprite2D/Area2D;
 
 var can_spawn = true
 var manager = GameState.current_stage.obstacles
 
+
 func _ready() -> void:
 	area.body_entered.connect(on_contact);
 	area.set_collision_mask_value(2, true); 
+
 
 func on_contact(_body: Node2D) -> void:
 	if can_spawn:
 		can_spawn = false
 		spawn_obstacles()
 
+
 func spawn_obstacles() -> void:
 	# LMG Note: Change into a timer, throw each one with 20ms in-between
 	# LMG Note: Spawn above trash can
-	var garbage: Node
-	for index in range(5):
-		garbage = manager.get_specific_obstacle("garbage")
-		garbage.linear_velocity = Vector2(randi_range(-10, 10), 10)
-		add_child(garbage)
+	for i in 5:
+		get_tree().create_timer(0.2 * i).timeout.connect(_create_garbage)
+
+
+func _create_garbage() -> void:
+	var garbage: RigidBodyObstacle = manager.get_specific_obstacle("garbage");
+	garbage.set_collision_layer_value(1, false);
+	garbage.position = position + Vector2(randi_range(-20, 20), -120.0)
+	
+	# changing linear speed directly leads to undefined behaviour
+	# if you apply impulse of {desired velocity} * {rigid body's mass} it by definition will give it the speed you want
+	garbage.apply_impulse(Vector2(randi_range(-150, 150), -450) * garbage.mass);
+	get_parent().add_child(garbage)
