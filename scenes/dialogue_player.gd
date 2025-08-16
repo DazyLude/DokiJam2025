b2a123011dc5eeb257e3cbfc5d6328ebecffb024
DazyLude <36658009+DazyLude@users.noreply.github.com>
2025-08-16 16:25:24 +0400

    dialogue player adjustments and additional animations dialogues

--- scenes/dialogue_player.gd
+++ scenes/dialogue_player.gd
@@ -4,12 +4,19 @@ extends Control
 signal dialogue_finished;
 
 
-@onready var character_sprites : Array[SpriteEmotional] = [$CharacterLeft, $CharacterRight];
+enum {
+	LEFT,
+	RIGHT
+}
+
+
+@onready var character_sprites : Array[SpriteEmotional] = [$CharacterLeft, $CharacterRight, $Character3]; # xdd
 @onready var dialogue_text = $TextBox/Text;
 @onready var speaker = $TextBox/Speaker;
-@onready var audio = [$AudioStreamPlayer, $AudioStreamPlayerDragoon];
+@onready var audio = [$AudioStreamPlayer, $AudioStreamPlayerDragoon, $AudioStreamPlayerDragoon]; # xddd
 
 var text_animation : Tween;
+var playing_animations : bool = false;
 var character_tweens : Array[Tween] = [];
 var normal_positions : Array[Vector2] = [];
 
@@ -27,14 +34,43 @@ const SPEAK_JUMP = Vector2(0, -50);
 
 
 func _ready() -> void:
-	$CharacterLeft.position = (get_viewport_rect().size - $CharacterLeft.get_rect().size * $CharacterLeft.scale ) * Vector2(0, 1.0) - SPEAK_JUMP;
-	$CharacterRight.position = (get_viewport_rect().size - $CharacterLeft.get_rect().size * $CharacterRight.scale) - SPEAK_JUMP;
-	
 	character_tweens.resize(character_sprites.size());
 	normal_positions.resize(character_sprites.size());
 	for i in character_sprites.size():
 		normal_positions[i] = character_sprites[i].position;
+
+
+func setup_ith_sprite(i: int, sprite: Sprite2D) -> void:
+	match i:
+		0: setup_left_sprite(sprite);
+		1: setup_right_sprite(sprite);
+		2: setup_centered_sprite(sprite);
 	
+	normal_positions[i] = sprite.position;
+
+
+func setup_left_sprite(sprite: Sprite2D) -> void:
+	sprite.position = get_left_pos(sprite);
+
+
+func setup_right_sprite(sprite: Sprite2D) -> void:
+	sprite.position = get_right_pos(sprite);
+
+
+func setup_centered_sprite(sprite: Sprite2D) -> void:
+	sprite.position = get_centered_pos(sprite);
+
+
+func get_centered_pos(sprite: Sprite2D) -> Vector2:
+	return (get_viewport_rect().size - sprite.get_rect().size * sprite.scale) * Vector2(0.5, 1.0) - SPEAK_JUMP;
+
+
+func get_left_pos(sprite: Sprite2D) -> Vector2:
+	return (get_viewport_rect().size - sprite.get_rect().size * sprite.scale ) * Vector2(0, 1.0) - SPEAK_JUMP;
+
+
+func get_right_pos(sprite: Sprite2D) -> Vector2:
+	return (get_viewport_rect().size - sprite.get_rect().size * sprite.scale) - SPEAK_JUMP;
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -50,9 +86,12 @@ func play_dialogue(data: Dictionary) -> void:
 
 
 func setup_characters(characters: Array) -> void:
+	for ch in character_sprites:
+		ch.hide();
+	
 	$Explosion.hide();
 	
-	for i in 2:
+	for i in characters.size():
 		var character_id = characters[i];
 		
 		node_per_character[character_id] = character_sprites[i];
@@ -60,7 +99,9 @@ func setup_characters(characters: Array) -> void:
 		
 		var character_data = IntermissionData.character_data[characters[i]];
 		name_per_character[character_id] = character_data["name"];
-		character_sprites[i].texture = load(character_data["base"]);
+		character_sprites[i].show();
+		var sprite_path = character_data["base"];
+		character_sprites[i].texture = load(sprite_path) if sprite_path != "" else null;
 		character_sprites[i].emotions.clear();
 		idx_per_character[character_id] = i;
 		voices_per_character[character_id] = character_data.get("speech", null);
@@ -69,12 +110,19 @@ func setup_characters(characters: Array) -> void:
 			var emotion_path = character_data["emotions"][emotion_id];
 			character_sprites[i].emotions.push_back(load(emotion_path) if emotion_path != "" else null);
 			emote_idxs_per_character[character_id][emotion_id] = character_sprites[i].emotions.size() - 1;
+		
+		setup_ith_sprite(i, character_sprites[i])
 
 
 func next() -> void:
+	if playing_animations:
+		return;
+	
 	if text_animation != null and text_animation.is_running():
 		skip_text_animation();
 		return;
+	
+	playing_animations = true;
 	
 	current_line += 1;
 	if current_line >= dialogue_array.size():
@@ -92,6 +140,38 @@ func next() -> void:
 	var stream : AudioStream;
 	var meta = dialogue_line[2] if dialogue_line.size() == 3 else {};
 	
+	# other things
+	if meta.get("flip", false):
+		flip(char_idx);
+	
+	# play animations
+	match meta:
+		_ when meta.has("swap"):
+			await play_explosion_animation_start()
+			var new_character = meta["swap"];
+			var character_data = IntermissionData.character_data[new_character];
+			var sprite_path = character_data["base"];
+			character_sprites[char_idx].texture = load(sprite_path) if sprite_path != "" else null;
+			setup_ith_sprite(LEFT, character_sprites[char_idx]);
+			play_expolosion_animation_end()
+		
+		_ when meta.has("walk in"):
+			var new_character = meta["walk in"]["character"];
+			var character_data = IntermissionData.character_data[new_character];
+			setup_ith_sprite(char_idx, character_sprites[char_idx]);
+			walk_in_animation_setup(meta["walk in"]["side"], char_idx);
+			character_sprites[char_idx].texture = load(character_data["base"]);
+			await play_walk_in_animation(meta["walk in"]["side"], char_idx);
+		
+		_ when meta.has("hide"):
+			if meta["hide"]:
+				await hide_character(char_idx);
+			else:
+				await unhide_character(char_idx);
+		_ when not meta.get("skip", false) and not meta.get("no nudge", false):
+			nudge_character(char_idx);
+	
+	# play audio
 	if meta.has("sfx"):
 		stream = Sounds.get_stream_by_id(meta["sfx"]);
 	else:
@@ -103,30 +183,34 @@ func next() -> void:
 			_:
 				stream = null;
 	
-	if meta.has("swap"):
-		await play_explosion_animation_start()
-		var new_character = meta["swap"];
-		var character_data = IntermissionData.character_data[new_character];
-		character_sprites[char_idx].texture = load(character_data["base"]);
-		play_expolosion_animation_end()
-	
 	audio[char_idx].stream = stream;
 	audio[char_idx].play();
 	
-	nudge_character(char_idx);
+	playing_animations = false;
+	
+	if meta.get("skip", false):
+		next();
+		return;
+	
 	play_text_animation();
 
 
-func nudge_character(idx: int) -> void:
+func get_fresh_character_tween(idx: int) -> Tween:
 	var tween := character_tweens[idx];
 	if tween != null:
 		tween.kill();
 	tween = create_tween();
+	character_tweens[idx] = tween;
+	return tween;
+
+
+func nudge_character(idx: int) -> void:
+	var tween := get_fresh_character_tween(idx);
 	
 	tween.tween_property(character_sprites[idx], ^"position", normal_positions[idx] + SPEAK_JUMP, 0.1);
 	tween.tween_property(character_sprites[idx], ^"position", normal_positions[idx], 0.1);
 	
-	character_tweens[idx] = tween;
+	await tween.finished;
 
 
 func skip_text_animation() -> void:
@@ -155,6 +239,7 @@ func play_explosion_animation_start() -> void:
 	
 	$Explosion.scale = Vector2(0.1, 0.1)
 	$Explosion.rotation = 0;
+	$Explosion.material.set_shader_parameter("color_filter", Vector4(1.0, 1.0, 1.0, 1.0));
 	$Explosion.show();
 	
 	tween.tween_property($Explosion, ^"scale", Vector2(0.6, 0.6), 0.1);
@@ -168,10 +253,72 @@ func play_expolosion_animation_end() -> void:
 	
 	tween.set_parallel();
 	tween.set_ease(Tween.EASE_OUT);
-	tween.set_trans(Tween.TRANS_EXPO);
+	tween.set_trans(Tween.TRANS_LINEAR);
 	
-	tween.tween_property($Explosion, ^"rotation", 0.4, 1.5).set_trans(Tween.TRANS_LINEAR);
+	tween.tween_property($Explosion, ^"rotation", 0.4, 1.5);
+	tween.tween_property($Explosion, ^"scale", Vector2(0.5, 0.5), 1.5);
 	tween.tween_property($Explosion.material, ^"shader_parameter/color_filter", Vector4(1.0, 1.0, 1.0, 0.0), 1.5);
 	
 	await tween.finished;
 	$Explosion.hide();
+
+
+func walk_in_animation_setup(side: int, idx: int) -> void:
+	pass;
+
+
+func play_walk_in_animation(side: int, idx: int) -> void:
+	var tween := get_fresh_character_tween(idx);
+	var sprite = character_sprites[idx];
+	var sprite_final_pos = get_left_pos(sprite) if side == LEFT else get_right_pos(sprite);
+	var width = sprite.get_rect().size * sprite.scale * Vector2(1.0, 0.0);
+	
+	normal_positions[idx] = sprite_final_pos;
+	
+	var sprite_starting_pos = get_left_pos(sprite) if side == LEFT else get_right_pos(sprite);
+	sprite_starting_pos += -width if side == LEFT else width;
+	
+	sprite.position = sprite_starting_pos;
+	
+	tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC);
+	tween.tween_property(sprite, ^"position", sprite_final_pos, 0.5);
+	
+	await tween.finished;
+
+
+func flip(idx: int) -> void:
+	character_sprites[idx].flip_h = not character_sprites[idx].flip_h;
+
+
+func hide_character(idx: int) -> void:
+	var tween := get_fresh_character_tween(idx);
+	var sprite = character_sprites[idx];
+	var height = sprite.get_rect().size * sprite.scale * Vector2(0.0, 1.0);
+	
+	var sprite_final_pos = get_centered_pos(sprite) + height * Vector2(1.0, 0.20);
+	normal_positions[idx] = sprite_final_pos;
+	
+	var down_pos = sprite.position + height;
+	var down_center_pos = sprite_final_pos + height;
+	
+	# go down
+	tween.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC);
+	tween.tween_property(sprite, ^"position", down_pos, 0.5);
+	tween.tween_property(sprite, ^"position", down_center_pos, 0.1);
+	tween.set_ease(Tween.EASE_OUT);
+	tween.tween_property(sprite, ^"position", sprite_final_pos, 0.5);
+	
+	await tween.finished;
+
+
+func unhide_character(idx: int) -> void:
+	var tween := get_fresh_character_tween(idx);
+	var sprite = character_sprites[idx];
+	var sprite_final_pos = get_centered_pos(sprite);
+	normal_positions[idx] = sprite_final_pos;
+	
+	# pop up
+	tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC);
+	tween.tween_property(sprite, ^"position", sprite_final_pos, 0.3);
+	
+	await tween.finished;
--- scripts/intermission_data.gd
+++ scripts/intermission_data.gd
@@ -1,5 +1,9 @@
 class_name IntermissionData
 
+
+const tm_string = '[font_size=24][b][i]™[/i][/b][/font_size]';
+
+
 enum {
 	TYPE_YONKOMA,
 	TYPE_DIALOGUE,
@@ -12,6 +16,10 @@ enum {
 	CHARACTER_RETRO,
 	CHARACTER_BOUNTY,
 	CHARACTER_DRAGOON,
+	CHARACTER_DRAGOON_FLIPPED,
+	
+	CHARACTER_EMPTY,
+	CHARACTER_EMPTY2,
 }
 
 enum {
@@ -54,6 +62,30 @@ static var character_data : Dictionary[int, Dictionary] = {
 	CHARACTER_DRAGOON: {
 		"base": "res://assets/intermissions/goons_sprite.png",
 		"name": "Dragoon",
+		"emotions": {
+			EMOTE_NORMAL: "",
+		},
+		"speech": Sounds.ID.VOX_OOH,
+	},
+	CHARACTER_DRAGOON_FLIPPED: {
+		"base": "res://assets/intermissions/goons_sprite_f.png",
+		"name": "Other Dragoon",
+		"emotions": {
+			EMOTE_NORMAL: "",
+		},
+		"speech": Sounds.ID.VOX_OOH,
+	},
+	CHARACTER_EMPTY: {
+		"base": "",
+		"name": "Dragoon",
+		"emotions": {
+			EMOTE_NORMAL: "",
+		},
+		"speech": Sounds.ID.VOX_OOH,
+	},
+	CHARACTER_EMPTY2: {
+		"base": "",
+		"name": "Other Dragoon",
 		"emotions": {
 			EMOTE_NORMAL: "",
 		},
@@ -138,7 +170,7 @@ static var intermission_data : Dictionary[String, Dictionary] = {
 			],
 			[
 				CHARACTER_DRAGOON,
-				"If you’re just looking for caffeine, there’s a DokiSupps[font_size=24][b][i]™[/i][/b][/font_size] dispensary in town you could try.",
+				"If you’re just looking for caffeine, there’s a DokiSupps%s dispensary in town you could try." % tm_string,
 				{"sfx": Sounds.ID.VOX_AAAH}
 			],
 			[
@@ -148,7 +180,7 @@ static var intermission_data : Dictionary[String, Dictionary] = {
 			],
 			[
 				CHARACTER_TOMATO,
-				"This must be a sign. DokiSupps[font_size=24][b][i]™[/i][/b][/font_size] here I come!",
+				"This must be a sign. DokiSupps%s here I come!" % tm_string,
 				{"sfx": Sounds.ID.VOX_AAH}
 			],
 			[
@@ -174,28 +206,220 @@ static var intermission_data : Dictionary[String, Dictionary] = {
 		"characters": [CHARACTER_CROWKI, CHARACTER_DRAGOON],
 		"bgm": Sounds.ID.MUSIC_GOOFY_AAH,
 		"dialogue": [
-			[CHARACTER_TOMATO, "hey"],
-			[CHARACTER_DRAGOON, "caw!"],
+			[
+				CHARACTER_DRAGOON,
+				"Hello, welcome to the DokiSupps%s dispensary, what can I help you with?" % tm_string,
+				{"sfx": Sounds.ID.VOX_SIGH}
+			],
+			[
+				CHARACTER_CROWKI,
+				"Give me everything you have.",
+				{"sfx": Sounds.ID.VOX_OOH}
+			],
+			[
+				CHARACTER_DRAGOON,
+				"Are you robbing me??",
+				{"sfx": Sounds.ID.SFX_AGGH}
+			],
+			[
+				CHARACTER_CROWKI,
+				"Of course not! See, I have money.",
+				{"sfx": Sounds.ID.VOX_UGHH}
+			],
+			[
+				CHARACTER_DRAGOON,
+				"Oh, ok, here you are!",
+				{"sfx": Sounds.ID.VOX_UEH}
+			],
+			[
+				CHARACTER_CROWKI,
+				"Yes, I need more! Do you have any more?",
+				{"swap": CHARACTER_RETRO, "sfx": Sounds.ID.VOX_LAUGH}
+			],
+			[
+				CHARACTER_DRAGOON,
+				"Nope, I sold you all I have. I didn’t even have that much left after I sold a bunch to Miss Fantome.",
+				{"sfx": Sounds.ID.VOX_NYOO}
+			],
+			[
+				CHARACTER_CROWKI,
+				"Who’s that?",
+				{"sfx": Sounds.ID.VOX_WHAT}
+			],
+			[
+				CHARACTER_DRAGOON,
+				"Oh she’s a local idol who has dreams of making it big. She’s performing today to try to keep everyone awake.",
+				{"sfx": Sounds.ID.VOX_AAH}
+			],
+			[
+				CHARACTER_DRAGOON,
+				"People’s coffee around town has been mysteriously disappearing recently and they’re making a big fuss.",
+				{"sfx": Sounds.ID.VOX_AAAH}
+			],
+			[
+				CHARACTER_DRAGOON,
+				"There’s only so much the energy drinks we supply can do for them.",
+				{"sfx": Sounds.ID.VOX_SIGH}
+			],
+			[
+				CHARACTER_DRAGOON,
+				"The only thing that can calm them down is Miss Fantome’s dulcet tones.",
+				{"sfx": Sounds.ID.VOX_SIGH}
+			],
+			[
+				CHARACTER_CROWKI,
+				"Cool story bro. Where is this performance happening?",
+				{"sfx": Sounds.ID.VOX_AIGHT}
+			],
+			[
+				CHARACTER_DRAGOON,
+				"Oh, it’s at the Arena in the middle of the city. About that way.",
+				{"sfx": Sounds.ID.VOX_OOH}
+			],
+			[
+				CHARACTER_CROWKI,
+				"Kay, thanks, byeee~",
+				{"sfx": Sounds.ID.VOX_LAUGH}
+			],
 		],
 	},
 	"vn3-4": {
 		"type": TYPE_DIALOGUE,
-		"background": "res://assets/stages/farm/tomato_farm.png",
-		"characters": [CHARACTER_RETRO, CHARACTER_DRAGOON],
+		"background": "res://assets/stages/backstage/backstage.png",
+		"characters": [CHARACTER_RETRO, CHARACTER_EMPTY, CHARACTER_EMPTY2],
 		"bgm": Sounds.ID.MUSIC_GOOFY_AAH,
 		"dialogue": [
-			[CHARACTER_TOMATO, "hey"],
-			[CHARACTER_DRAGOON, "caw!"],
+			[
+				CHARACTER_RETRO,
+				"This must be the arena. Let’s see what we have here…",
+				{"sfx": Sounds.ID.VOX_UGHH}
+			],
+			[
+				CHARACTER_RETRO,
+				"Oh boy, I must have hit the jackpot! There’s so much DokiSupps%s here." % tm_string,
+				{"sfx": Sounds.ID.VOX_LAUGH}
+			],
+			[
+				CHARACTER_RETRO,
+				"Yum yum.",
+				{"sfx": Sounds.ID.VOX_OOH}
+			],
+			[
+				CHARACTER_RETRO,
+				"I think I’ve reached my final form!",
+				{"swap": CHARACTER_BOUNTY, "sfx": Sounds.ID.VOX_LAUGH}
+			],
+			[
+				CHARACTER_RETRO,
+				"Wait, someone’s coming. I gotta hide.",
+				{"sfx": Sounds.ID.VOX_AAAH}
+			],
+			[
+				CHARACTER_RETRO,
+				"",
+				{"hide": true, "skip": true}
+			],
+			[
+				CHARACTER_EMPTY,
+				"I know Miss Fantome gets cranky when she hasn’t had caffeine in a while, but did we really have to keep it so far away?",
+				{"walk in": {"side": 1, "character": CHARACTER_DRAGOON}, "sfx": Sounds.ID.VOX_SIGH}
+			],
+			[CHARACTER_RETRO, "", {"skip": true, "flip": true}],
+			[
+				CHARACTER_EMPTY2,
+				"And saying she won’t perform if she doesn’t get any soon? How needy can one person get?",
+				{"walk in": {"side": 0, "character": CHARACTER_DRAGOON_FLIPPED}, "sfx": Sounds.ID.VOX_UGHH}
+			],
+			[
+				CHARACTER_RETRO,
+				"Heh heh, what a sucker.",
+				{"sfx": Sounds.ID.VOX_EH}
+			],
+			[CHARACTER_RETRO, "", {"skip": true, "flip": true}],
+			[
+				CHARACTER_EMPTY,
+				"Oh nonononononononono.",
+				{"sfx": Sounds.ID.VOX_NYOO}
+			],
+			[
+				CHARACTER_EMPTY,
+				"How can we possibly be out of DokiSupps%s already? We just bought some more." % tm_string,
+				{"sfx": Sounds.ID.VOX_NYOO}
+			],
+			[
+				CHARACTER_EMPTY,
+				"Now Miss Fantome is going to kill me, the wisps are going to riot!",
+				{"sfx": Sounds.ID.VOX_EH}
+			],
+			[CHARACTER_RETRO, "", {"skip": true, "flip": true}],
+			[
+				CHARACTER_EMPTY2,
+				"And more importantly, this concert is going to be cancelled!",
+				{"sfx": Sounds.ID.VOX_NYOO}
+			],
+			[
+				CHARACTER_EMPTY2,
+				"Where are we supposed to find a replacement?",
+				{"sfx": Sounds.ID.VOX_SIGH}
+			],
+			[
+				CHARACTER_EMPTY2,
+				"We can’t even pay anyone with anything other than a dog.",
+				{"sfx": Sounds.ID.VOX_SIGH}
+			],
+			[
+				CHARACTER_RETRO,
+				"Wait, I can sing and I get a dog too?",
+				{"hide": false, "sfx": Sounds.ID.VOX_WHAT}
+			],
+			[
+				CHARACTER_EMPTY2,
+				"Ahhhhhhh! Who are you?!",
+				{"sfx": Sounds.ID.SFX_AGGH}
+			],
+			[
+				CHARACTER_RETRO,
+				"Don’t worry about it, show me where the stage is. And get me that dog!",
+				{"flip": true, "sfx": Sounds.ID.VOX_LAUGH}
+			],
 		],
 	},
 	"vn4-5": {
 		"type": TYPE_DIALOGUE,
-		"background": "res://assets/stages/farm/tomato_farm.png",
+		"background": "res://assets/stages/stage/stage.png",
 		"characters": [CHARACTER_BOUNTY, CHARACTER_DRAGOON],
 		"bgm": Sounds.ID.MUSIC_GOOFY_AAH,
 		"dialogue": [
-			[CHARACTER_TOMATO, "hey"],
-			[CHARACTER_DRAGOON, "caw!"],
+			[
+				CHARACTER_DRAGOON,
+				"Here we are then. Hopefully you can actually sing, miss.",
+				{"sfx": Sounds.ID.VOX_AAH}
+			],
+			[
+				CHARACTER_BOUNTY,
+				"Don’t worry about it.",
+				{"sfx": Sounds.ID.VOX_EH}
+			],
+			[
+				CHARACTER_DRAGOON,
+				"Before you go on, we were planning on sending Miss Fantome on tour.",
+				{"sfx": Sounds.ID.VOX_AAH}
+			],
+			[
+				CHARACTER_DRAGOON,
+				"If this goes well, we can send you instead.",
+				{"sfx": Sounds.ID.VOX_AAAH}
+			],
+			[
+				CHARACTER_BOUNTY,
+				"Alright bet. Make sure you prepare some Doki World Tour posters!",
+				{"sfx": Sounds.ID.VOX_LAUGH}
+			],
+			[
+				CHARACTER_DRAGOON,
+				"Break a leg. Or, well, we’d really prefer if you didn’t.",
+				{"sfx": Sounds.ID.VOX_SIGH}
+			],
 		],
 	},
 }
